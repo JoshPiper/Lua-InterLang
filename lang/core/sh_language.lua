136d19022a4755bb1abc3d569d23e7ae1f56ab50
John Internet <jonjon1234.github@gmail.com>
2020-08-16 20:18:49 +0100

    Add language docs.

--- lang/core/sh_language.lua
+++ lang/core/sh_language.lua
@@ -1,4 +1,8 @@
 --[[-- License Plate Language Handler
+
+The language handler represents a single language, with a set of translation strings and plurals.
+The language also contains details, such as the used plural rules.
+
 @copyright 2020 Joshua Piper
 @release development
 @author Joshua Piper
@@ -9,25 +13,36 @@ AddCSLuaFile()
 local lang = {}
 lang.__index = lang
 
+--- Set a language variable.
+-- @string key Unique ID
+-- @string phrase Localised phrase.
 function lang:__call(key, phrase)
 	self.stored[key or "#"] = (phrase or "?")
 end
 
 local plural = include("sh_plural.lua")
+--- Create a new plural.
+-- @treturn plural
 function lang:NewPlural()
 	return setmetatable({parent = self}, plural)
 end
 
+--- Generate a plural based on input params
+-- @see plural.Set
+-- @tparam ... id and plural forms.
 function lang:Plural(...)
 	self:NewPlural()
 		:Set(...)
 		:Register()
 end
 
+--- Register a plural.
+-- @tparam plural newPlural
 function lang:RegisterPlural(newPlural)
 	self.plurals[newPlural.id] = newPlural
 end
 
+--- Register this language in the languages table.
 function lang:Register()
 	self.parent:Register(self)
 end
