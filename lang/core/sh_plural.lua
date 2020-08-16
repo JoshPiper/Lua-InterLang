3da6fdf7bad33ad38a9c301a64ca98c42f378090
John Internet <jonjon1234.github@gmail.com>
2020-08-16 20:15:11 +0100

    Set them to be the same, don't nest calls.

--- lang/core/sh_plural.lua
+++ lang/core/sh_plural.lua
@@ -12,15 +12,20 @@ AddCSLuaFile()
 local plural = {}
 plural.__index = plural
 
+--- Set the data for this plural.
+-- @string id Unique ID to set for this plural, for which it can be referenced by language files.
+-- @tparam vararg ... String plural forms, listed in order that the language plural rule demands.
+-- @treturn plural
 function plural:Set(id, ...)
 	self.id = id
 	self.set = {...}
 	return self
 end
 
-function plural:__call(...)
-	return self:Set(...)
-end
+--- Duplicate of Set.
+-- @see plural.Set
+-- @function plural.__call
+plural.__call = plural.Set
 
 function plural:Check(num)
 	return self.set[self.parent.pluralRule:Check(num)]
